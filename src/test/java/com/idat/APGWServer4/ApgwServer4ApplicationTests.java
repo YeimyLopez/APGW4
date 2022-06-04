0187e195de0a8952a4113a5c865a3bc2799ac255
Piero Nicolas Quispe Pozo <pieronicolas333@gmail.com>
2022-06-04 00:03:56 -0500

    APHW - Piero Nicolas Quispe Pozo

---
+++ src/test/java/com/idat/APGWServer4/ApgwServer4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.APGWServer4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApgwServer4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
